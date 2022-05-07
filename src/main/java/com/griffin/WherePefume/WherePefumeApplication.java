d12aa2e045fb3766ed66deaf36fcd1dd66d3c097
griffinGC <griffindouble@gmail.com>
2022-05-08 00:52:25 +0900

    Initial commit

---
+++ src/main/java/com/griffin/WherePefume/WherePefumeApplication.java
@@ -0,0 +1,13 @@
+package com.griffin.WherePefume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WherePefumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WherePefumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griffin/WherePefume/WherePefumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griffin.WherePefume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WherePefumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
